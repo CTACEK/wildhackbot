dd19ef9e04b20858219908a9767f5e41b6767f36
Яна <yanina.exessa@yandex.ru>
2021-12-04 17:03:47 +0300

    new db + connection with new columns

--- db.py
+++ db.py
@@ -5,10 +5,12 @@ class BotDB:
     def __init__(self, db_file):
         self.conn = sqlite3.connect(db_file)
         self.cursor = self.conn.cursor()
+        print(self.cursor.execute("SELECT * FROM 'users'").fetchall())
 
     def user_exists(self, user_id):
         """Проверяем, есть ли юзер в базе"""
-        result = self.cursor.execute("SELECT id FROM users WHERE user_id = 123")
+        result = self.cursor.execute("SELECT `id` FROM `users` WHERE `user_id` = ?", (user_id,))
+
         return bool(len(result.fetchall()))
 
     def get_user_id(self, user_id):
@@ -21,31 +23,14 @@ class BotDB:
         self.cursor.execute("INSERT INTO users (user_id) VALUES (?)", (user_id,))
         return self.conn.commit()
 
-    def add_record(self, user_id, operation, value):
-        """Создаем запись о доходах/расходах"""
-        self.cursor.execute("INSERT INTO `records` (`users_id`, `operation`, `value`) VALUES (?, ?, ?)",
-            (self.get_user_id(user_id),
-            operation == "+",
-            value))
+
+    def add_information(self, user_id, columns, value):
+        """Добавляем информацию о пользователе"""
+        columns = [""]
+        name_of_columns = ""
+        self.cursor.execute("UPDATE `users` SET ? = ? WHERE id = ?",
+            (name_of_columns, value, user_id,))
         return self.conn.commit()
-
-    def get_records(self, user_id, within = "all"):
-        """Получаем историю о доходах/расходах"""
-
-        if(within == "day"):
-            result = self.cursor.execute("SELECT * FROM `records` WHERE `users_id` = ? AND `date` BETWEEN datetime('now', 'start of day') AND datetime('now', 'localtime') ORDER BY `date`",
-                (self.get_user_id(user_id),))
-        elif(within == "week"):
-            result = self.cursor.execute("SELECT * FROM `records` WHERE `users_id` = ? AND `date` BETWEEN datetime('now', '-6 days') AND datetime('now', 'localtime') ORDER BY `date`",
-                (self.get_user_id(user_id),))
-        elif(within == "month"):
-            result = self.cursor.execute("SELECT * FROM `records` WHERE `users_id` = ? AND `date` BETWEEN datetime('now', 'start of month') AND datetime('now', 'localtime') ORDER BY `date`",
-                (self.get_user_id(user_id),))
-        else:
-            result = self.cursor.execute("SELECT * FROM `records` WHERE `users_id` = ? ORDER BY `date`",
-                (self.get_user_id(user_id),))
-
-        return result.fetchall()
 
     def close(self):
         """Закрываем соединение с БД"""
