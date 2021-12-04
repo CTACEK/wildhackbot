d0635cdbed989a0ef9a0915a989b60d897da3be1
Яна <yanina.exessa@yandex.ru>
2021-12-04 21:30:45 +0300

    added anket fulling with db connect

--- db.py
+++ db.py
@@ -5,7 +5,6 @@ class BotDB:
     def __init__(self, db_file):
         self.conn = sqlite3.connect(db_file)
         self.cursor = self.conn.cursor()
-        print(self.cursor.execute("SELECT * FROM 'users'").fetchall())
 
     def user_exists(self, user_id):
         """Проверяем, есть ли юзер в базе"""
@@ -20,17 +19,20 @@ class BotDB:
 
     def add_user(self, user_id):
         """Добавляем юзера в базу"""
-        self.cursor.execute("INSERT INTO users (user_id) VALUES (?)", (user_id,))
+        self.cursor.execute("INSERT INTO users (user_id) VALUES (?);", (user_id,))
         return self.conn.commit()
 
 
-    def add_information(self, user_id, columns, value):
+    def add_information(self, user_id, name_of_column, value):
         """Добавляем информацию о пользователе"""
-        columns = [""]
-        name_of_columns = ""
-        self.cursor.execute("UPDATE `users` SET ? = ? WHERE id = ?",
-            (name_of_columns, value, user_id,))
+        #columns = ["user_id", "full_name", "date_of_application", "mail", "birthday", "phone_number", "education",
+        #           "territory", "arrival_date", "departure_date", "lang", "experience", "skills", "recommendations",
+        #           "volunteer_book", "pitch", "video", "reviewed"]
+
+        self.cursor.execute("UPDATE users SET '{0}' = '{1}' WHERE user_id = '{2}';".format(name_of_column, value, user_id))
+
         return self.conn.commit()
+
 
     def close(self):
         """Закрываем соединение с БД"""
--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -5,8 +5,10 @@ from dispatcher import dp
 import config
 import re
 from bot import BotDB
-from aiogram import Bot
-from keyboards import menu
+from aiogram import Bot, types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
 
 
 @dp.message_handler(commands="start")
@@ -27,20 +29,54 @@ async def start(message: types.Message):
     await message.answer((f"Чего желаете, {message.from_user.first_name}?"), reply_markup=keyboard)
 
 
-@dp.message_handler(lambda message: message.text == "Заполнить заявку")
-async def without_puree(message: types.Message):
-
-    await message.answer("Для заполнения анкеты напишите свое ФИО")
-
-
-
-
 @dp.message_handler(lambda message: message.text == "FAQ")
 async def without_puree(message: types.Message):
-    await message.answer("Фак ю")
+    await message.answer("Факью")
 
 
 @dp.message_handler(lambda message: message.text == "О заповеднике")
 async def without_puree(message: types.Message):
     await message.answer("Бла бла бла")
+
+
+
+# Заполнение анкеты
+
+class FSMAnket(StatesGroup):
+    columns = {"user_id": State(), "full_name": State(), "date_of_application": State(), "mail": State(),
+               "birthday": State(), "phone_number": State(), "education": State(), "territory": State(),
+               "arrival_date": State(), "departure_date": State(), "lang": State(), "experience": State(),
+               "skills": State(), "recommendations": State(), "volunteer_book": State(), "pitch": State(),
+               "video": State(), "reviewed": State()}
+
+
+# Начало диалога
+@dp.message_handler(lambda message: message.text == "Заполнить заявку", state=None)
+async def cm_start(message: types.Message):
+    await FSMAnket.columns.get("full_name").set()
+    await message.answer("Введите ФИО")
+
+
+# Грузим ФИО
+@dp.message_handler(state=FSMAnket.columns.get("full_name"))
+async def load_fio(message: types.Message, state: FSMContext):
+    BotDB.add_information(message.from_user.id, "full_name", message.text)
+    await FSMAnket.next()
+    await message.answer("Введите почту")
+
+
+# Ловим второй ответ - почту
+@dp.message_handler(state=FSMAnket.columns.get("mail"))
+async def load_mail(message: types.Message, state: FSMContext):
+    BotDB.add_information(message.from_user.id, "mail", message.text)
+    await FSMAnket.next()
+    await message.answer("B")
+
+@dp.message_handler(state=FSMAnket.columns.get("mail"))
+async def load_mail(message: types.Message, state: FSMContext):
+    # загрузка в БД
+
+    await FSMAnket.next()
+    await message.answer("Введите чото-там")
+
 
